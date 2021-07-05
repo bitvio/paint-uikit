3282e5b3a671135dc5fde81190c95b051f729416
homebitcoin <richo.library@gmail.com>
2021-07-05 13:13:43 +0700

    Logo Mobile

--- src/widgets/Menu/Logo.tsx
+++ src/widgets/Menu/Logo.tsx
@@ -24,7 +24,7 @@ const StyledLink = styled(Link)`
   }
   .desktop-icon {
     width: 156px;
-    display: none;
+    display: block;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: block;
     }
@@ -50,11 +50,11 @@ const Logo: React.FC<Props> = ({ isPushed, togglePush, isDark, href }) => {
         )}
       </MenuButton>
       {isAbsoluteUrl ? (
-        <StyledLink as="a" href={href} aria-label="Pancake home page">
+        <StyledLink as="a" href={href} aria-label="Bitvio home page">
           {innerLogo}
         </StyledLink>
       ) : (
-        <StyledLink to={href} aria-label="Pancake home page">
+        <StyledLink to={href} aria-label="Bitvio home page">
           {innerLogo}
         </StyledLink>
       )}
