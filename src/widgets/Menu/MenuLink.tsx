e35a450068324e4d4d709c93bcb10169355005de
homebitcoin <richo.library@gmail.com>
2021-07-05 16:49:20 +0700

    Menu Link

--- src/widgets/Menu/MenuLink.tsx
+++ src/widgets/Menu/MenuLink.tsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...otherProps }) => {
   const isHttpLink = href?.startsWith("http");
 
-  const isBitvio =
+  const isZCore =
     href?.startsWith("https://exchange.bitvio.id") ||
-    href?.startsWith("https://apps.bitvio.id") ||
+    href?.startsWith("https://app.bitvio.id") ||
     href?.startsWith("https://finance.bitvio.id") ||
     href?.startsWith("/") ||
     href?.startsWith("https://swap.bitvio.id");
@@ -14,7 +14,7 @@ const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Tag: any = isHttpLink ? "a" : NavLink;
   const props = isHttpLink ? { href } : { to: href };
-  if (isBitvio) {
+  if (isZCore) {
     return <Tag {...props} {...otherProps} />;
   } else {
     return <Tag target="_blank" {...props} {...otherProps} />;
